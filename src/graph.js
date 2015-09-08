d80ac846dbe850f8a81f4c6bff7878da42be29eb
Daniela Baron <baron.daniela@gmail.com>
2015-09-08 19:47:42 -0400

    d3 updating pattern

--- src/graph.js
+++ src/graph.js
@@ -7,24 +7,43 @@ export class Graph {
 
   constructor(element) {
     this.element = element;
+    this.svg = d3.select(this.element)
+      .append('svg')
+      .attr('width', 960)
+      .attr('height', 500);
   }
 
-  valueChanged(newValue){
-    if (newValue) {
-      console.log('*** graph newValue = ' + newValue);
-      var sampleSVG = d3.select(this.element)
-          .append("svg")
-          .attr("width", 100)
-          .attr("height", 100);
+  update(data) {
+    var circles = this.svg.selectAll('circle')
+      .data(data);
 
-      sampleSVG.append("circle")
-          .style("stroke", "gray")
-          .style("fill", "white")
-          .attr("r", 40)
-          .attr("cx", 50)
-          .attr("cy", 50)
-          .on("mouseover", function(){d3.select(this).style("fill", "aliceblue");})
-          .on("mouseout", function(){d3.select(this).style("fill", "white");});
+    // UPDATE: Update old elements as needed.
+    circles.attr('class', 'update');
+
+    // ENTER: Create new elements as needed.
+    circles.enter().append('circle')
+      .style('stroke', 'gray')
+      .style('fill', 'white')
+      .attr('r', 40)
+      .attr('cx', function(d, i) { return i * 50; })
+      .attr('cy', 50)
+      .on('mouseover', function(){d3.select(this).style('fill', 'aliceblue');})
+      .on('mouseout', function(){d3.select(this).style('fill', 'white');});
+
+    // ENTER + UPDATE
+    // Appending to the enter selection expands the update selection to include
+    // entering elements; so, operations on the update selection after appending to
+    // the enter selection will apply to both entering and updating nodes.
+    circles.text(function(d) { return d; })
+      .style("font-size", "12px");
+
+    // EXIT: Remove old elements as needed.
+    circles.exit().remove();
+  }
+
+  valueChanged(newValue) {
+    if (newValue) {
+      this.update(newValue.split(''));
     }
-    }
+  }
 }
