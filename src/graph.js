224efde34dc48559b146a372d8ea9b3690729979
Daniela Baron <baron.daniela@gmail.com>
2015-09-08 20:06:39 -0400

    demonstrate input from UI displayed in d3 viz

--- src/graph.js
+++ src/graph.js
@@ -14,31 +14,25 @@ export class Graph {
   }
 
   update(data) {
-    var circles = this.svg.selectAll('circle')
+    let groups = this.svg.selectAll('g')
       .data(data);
 
-    // UPDATE: Update old elements as needed.
-    circles.attr('class', 'update');
+    let groupsEnter = groups.enter()
+      .append('g')
+      .attr("transform", function(d, i){return "translate("+i*80+",80)";});
 
-    // ENTER: Create new elements as needed.
-    circles.enter().append('circle')
+    groupsEnter.append('circle')
       .style('stroke', 'gray')
       .style('fill', 'white')
       .attr('r', 40)
-      .attr('cx', function(d, i) { return i * 50; })
-      .attr('cy', 50)
       .on('mouseover', function(){d3.select(this).style('fill', 'aliceblue');})
       .on('mouseout', function(){d3.select(this).style('fill', 'white');});
 
-    // ENTER + UPDATE
-    // Appending to the enter selection expands the update selection to include
-    // entering elements; so, operations on the update selection after appending to
-    // the enter selection will apply to both entering and updating nodes.
-    circles.text(function(d) { return d; })
-      .style("font-size", "12px");
+    groupsEnter.append('text')
+      .attr('dx', function(d){return -20;})
+      .text(function(d){return d;});
 
-    // EXIT: Remove old elements as needed.
-    circles.exit().remove();
+    groups.exit().remove();
   }
 
   valueChanged(newValue) {
