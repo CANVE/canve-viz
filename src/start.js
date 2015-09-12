bac35f33c8b48f20c7e99cb795f297cb69ee5370
Daniela Baron <baron.daniela@gmail.com>
2015-09-11 20:32:25 -0400

    http fetch client with csv data

--- src/start.js
+++ src/start.js
@@ -1,8 +1,32 @@
-export class Start {
-  heading = 'Welcome to the Aurelia Navigation App!';
-  someData = [0, 1, 2];
+import {inject} from 'aurelia-framework';
+import {HttpClient} from 'aurelia-fetch-client';
+import 'fetch';
 
-  constructor(){
-    console.log('*** Start view is running');
+@inject(HttpClient)
+export class Start {
+  graphData = {};
+
+  // TODO baseUrl should be configurable
+  // Works given that serverLocalCORS.py is started in canve/visualizer
+  constructor(http){
+    http.configure(config => {
+      config
+        .withBaseUrl('http://localhost:31338/');
+    });
+
+    this.http = http;
   }
+
+  // TODO Also need edges and some data cleaning - service?
+  // TODO Background task for loading node-source files?
+  activate() {
+    return this.http.fetch('canve-data/nodes', { headers: { 'Content-Type': 'text/plain' } })
+      .then(response => response.text())
+      .then(nodes => {
+        this.graphData.nodes = nodes;
+        // TODO csv parsing, try: http://papaparse.com/
+        console.dir(this.graphData);
+      });
+  }
+
 }
