5142fea9ba35932d83ac26ce4ec3642732e8cd7b
Daniela Baron <baron.daniela@gmail.com>
2015-09-13 13:27:55 -0400

    add multiple bind values on graph to distinguish data change vs query change

--- src/graph.js
+++ src/graph.js
@@ -1,4 +1,4 @@
-import {inject, customAttribute} from 'aurelia-framework';
+import {inject, customAttribute, bindable} from 'aurelia-framework';
 import {EventAggregator} from 'aurelia-event-aggregator';
 import d3 from 'd3';
 import GraphUtils from 'graph-utils';
@@ -6,6 +6,8 @@ import GraphUtils from 'graph-utils';
 @customAttribute('graph')
 @inject(Element, EventAggregator, GraphUtils)
 export class Graph {
+  @bindable data;
+  @bindable query;
 
   constructor(element, pubSub, graphUtils) {
     this.element = element;
@@ -78,33 +80,21 @@ export class Graph {
   }
 
   // TODO: Bring in updateForceLayout from visualizer
-  update(data) {
-    // let groups = this.svg.selectAll('g')
-    //   .data(data);
-    //
-    // let groupsEnter = groups.enter()
-    //   .append('g')
-    //   .attr("transform", function(d, i){return "translate("+i*80+",80)";});
-    //
-    // groupsEnter.append('circle')
-    //   .style('stroke', 'gray')
-    //   .style('fill', 'white')
-    //   .attr('r', 40)
-    //   .on('mouseover', function(){d3.select(this).style('fill', 'aliceblue');})
-    //   .on('mouseout', function(){d3.select(this).style('fill', 'white');})
-    //   .on('click', d => this.pubSub.publish('node.clicked', d));
-    //
-    // groupsEnter.append('text')
-    //   .attr('dx', function(d){return -20;})
-    //   .text(function(d){return d.name;});
-    //
-    // groups.exit().remove();
+  updateForceLayout(d3GraphData) {
+
   }
 
-  valueChanged(newValue) {
+  dataChanged(newValue) {
+    console.log('dataChanged');
     if (newValue) {
       let d3GraphData = this.graphUtils.mapToD3(newValue);
-      this.update(d3GraphData.nodes);
+      this.updateForceLayout(d3GraphData);
+    }
+  }
+
+  queryChanged(newValue) {
+    if (newValue) {
+      console.log(`queryChanged: ${newValue}`);
     }
   }
 }
