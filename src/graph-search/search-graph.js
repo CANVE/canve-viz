3b92571a9181374799ec1a3cf621130a80130447
Daniela Baron <baron.daniela@gmail.com>
2015-10-05 19:39:06 -0400

    trying to import awesomeplete as module

--- src/graph-search/search-graph.js
+++ src/graph-search/search-graph.js
@@ -1,13 +1,15 @@
 import {inject} from 'aurelia-framework';
 import {customElement, bindable} from 'aurelia-framework';
 import {EventAggregator} from 'aurelia-event-aggregator';
+import * as Awesomplete from 'npm:awesomplete@1.0.0/awesomplete.js';
 
 @customElement('search-graph')
-@inject(EventAggregator)
+@inject(Element, EventAggregator)
 export class SearchGraph {
   @bindable data;
 
-  constructor(pubSub) {
+  constructor(element, pubSub) {
+    this.element = element;
     this.pubSub = pubSub;
     // TODO somewhere publish user's selected search query
   }
@@ -15,6 +17,11 @@ export class SearchGraph {
   dataChanged(newVal) {
     console.log('search-graph newVal:');
     console.dir(newVal);
+    let nodeNames = newVal.map( node => {
+      return node.name;
+    });
+    // new Awesomplete.constructor(this.element, { list: nodeNames });
+    Awesomplete.constructor(this.element, { list: nodeNames });
   }
 
 }
