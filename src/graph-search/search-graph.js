d439704463ec8705b5b57804bdbea80b55ddb836
Daniela Baron <baron.daniela@gmail.com>
2015-10-06 19:58:00 -0400

    use displayName and add event handler for selection

--- src/graph-search/search-graph.js
+++ src/graph-search/search-graph.js
@@ -1,7 +1,6 @@
 import {inject} from 'aurelia-framework';
 import {customElement, bindable} from 'aurelia-framework';
 import {EventAggregator} from 'aurelia-event-aggregator';
-// import * as Awesomplete from 'npm:awesomplete@1.0.0/awesomplete.js';
 import $ from 'jquery';
 import 'npm:awesomplete@1.0.0/awesomplete.js';
 
@@ -13,20 +12,21 @@ export class SearchGraph {
   constructor(element, pubSub) {
     this.element = element;
     this.pubSub = pubSub;
-    // TODO somewhere publish user's selected search query
   }
 
   dataChanged(newVal) {
-    console.log('search-graph newVal:');
-    console.dir(newVal);
     let nodeNames = newVal.map( node => {
-      return node.name;
+      return node.displayName;
     });
     new Awesomplete(
       $(this.element).find('input').get(0),
       { list: nodeNames }
     );
-    // Awesomplete.constructor(this.element, { list: nodeNames });
+    $(this.element).find('input').get(0).addEventListener('awesomplete-selectcomplete', () => {
+      let selectedVal = $(this.element).find('input').val();
+      console.log(`awesomeplete value selected: ${selectedVal}`);
+      // TODO publish user's selected search query
+    });
   }
 
 }
