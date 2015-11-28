a8f9a47a729b02780f17e878a1bb64e25f31a07d
Daniela Baron <baron.daniela@gmail.com>
2015-11-28 14:11:26 -0500

    use tweenmax from jspm rather than cdn

--- src/node/node.js
+++ src/node/node.js
@@ -1,8 +1,7 @@
 import {inject} from 'aurelia-framework';
 import {customElement, bindable, containerless} from 'aurelia-framework';
 import $ from 'jquery';
-// import 'npm:gsap@1.18.0/src/minified/TweenMax.min.js';
-// import 'npm:gsap@1.18.0/src/minified/plugins/AttrPlugin.min.js';
+import 'npm:gsap@1.18.0/src/minified/TweenMax.min.js';
 
 @customElement('node')
 @containerless
@@ -18,16 +17,6 @@ export class Node {
     if (newVal) {
       this.displayNode = newVal;
       console.dir(this.displayNode);
-      // TODO Delegate to node display helper to determine fill color
-
-      // Experiment with GSAP animation
-      // let $node = $('.node');
-      let $node = $(this.element).find('.node');
-      // let $domNode = $node.get(0);
-      // TweenLite.from(this.element, 10, {attr:{cx:0, cy:0}, ease: Linear.easeNone});
-      // TweenLite.from($node, 10, {attr:{cx:0, cy:0}, ease: Linear.easeNone});
-
-
     }
   }
 
@@ -35,11 +24,6 @@ export class Node {
     let nodeId = `#node-${this.displayNode.id}`;
     let $node = $(`#node-${this.displayNode.id}`);
     console.dir($node[0]);
-    // TweenLite.to('.node', 3, {attr:{cx:0, cy:0}, ease: Linear.easeNone});
-    // TweenLite.from('.node', 3, {attr:{cx:this.displayNode.x, cy:this.displayNode.y}, ease: Linear.easeNone});
-    // TweenLite.from($node[0], 3, {attr:{cx:this.displayNode.x, cy:this.displayNode.y}, ease: Linear.easeNone});
-    // TweenLite.to('.node', 10, {attr:{cx:this.displayNode.x, cy:this.displayNode.y}, ease: Linear.easeNone});
-    // TweenLite.to('#node-' + this.displayNode.id, 10, {attr:{cx:this.displayNode.x, cy:this.displayNode.y}, ease: Linear.easeNone});
 
     TweenLite.fromTo($node[0], 3,
       {attr: {cx:0, cy:0}},
