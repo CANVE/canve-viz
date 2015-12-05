c3568ed07d20520dd3ca866de8e89a2f7f720d8d
Daniela Baron <baron.daniela@gmail.com>
2015-12-05 16:19:58 -0500

    animate transform for node group svg element

--- src/node/node.js
+++ src/node/node.js
@@ -1,5 +1,8 @@
-import {inject} from 'aurelia-framework';
-import {customElement, bindable, containerless} from 'aurelia-framework';
+import {inject, customElement, bindable, containerless} from 'aurelia-framework';
+
+// bindingEngine is used statically, in the next Aurelia release it will be BindingEngine and injectable
+import {bindingEngine} from 'aurelia-binding';
+
 import $ from 'jquery';
 import 'npm:gsap@1.18.0/src/minified/TweenMax.min.js';
 
@@ -16,7 +19,14 @@ export class Node {
   dataChanged(newVal) {
     if (newVal) {
       this.displayNode = newVal;
-      // console.dir(this.displayNode);
+
+      bindingEngine.propertyObserver(this.displayNode, 'x').subscribe((newValue, oldValue) => {
+        this.animateX(this.$node, oldValue, newValue);
+      });
+
+      bindingEngine.propertyObserver(this.displayNode, 'y').subscribe((newValue, oldValue) => {
+        this.animateY(this.$node, oldValue, newValue);
+      });
     }
   }
 
@@ -26,12 +36,26 @@ export class Node {
 
   attached() {
     // Selector
-    let $node = $(`#node-${this.displayNode.id}`);
+    this.$node = $(`#node-${this.displayNode.id}`);
 
     // Animate into position
-    TweenLite.fromTo($node[0], 1,
-      {attr: {cx: this.displayNode.px, cy: this.displayNode.py}},
-      {attr: {cx: this.displayNode.x, cy: this.displayNode.y}, ease: Power1.easeIn}
+    TweenLite.fromTo(this.$node[0], 1,
+      {attr: {transform: `translate(${this.displayNode.px}, ${this.displayNode.py})`}},
+      {attr: {transform: `translate(${this.displayNode.x}, ${this.displayNode.y})`}, ease: Power1.easeIn}
+    );
+  }
+
+  animateX(selector, fromPos, toPos) {
+    TweenLite.fromTo(selector[0], 1,
+      {attr: {transform: `translate(${fromPos}, ${this.displayNode.y})`}},
+      {attr: {transform: `translate(${toPos}, ${this.displayNode.y})`}, ease: Power1.easeIn}
+    );
+  }
+
+  animateY(selector, fromPos, toPos) {
+    TweenLite.fromTo(selector[0], 1,
+      {attr: {transform: `translate(${this.displayNode.x}, ${fromPos})`}},
+      {attr: {transform: `translate(${this.displayNode.x}, ${toPos})`}, ease: Power1.easeIn}
     );
   }
 
