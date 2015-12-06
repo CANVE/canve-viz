96c064276922c07209678152f4d01e3ac4f31762
Daniela Baron <baron.daniela@gmail.com>
2015-12-06 07:24:07 -0500

    animate initial line position from source to target node

--- src/edge/edge.js
+++ src/edge/edge.js
@@ -1,4 +1,6 @@
 import {inject, customElement, bindable, containerless} from 'aurelia-framework';
+import $ from 'jquery';
+import 'npm:gsap@1.18.0/src/minified/TweenMax.min.js';
 
 @customElement('edge')
 @containerless
@@ -14,5 +16,16 @@ export class Edge {
     if (newVal) {
       this.displayEdge = newVal;
     }
+  }
+
+  attached() {
+    // Selector
+    this.$edge = $(`#edge-${this.displayEdge.source.id}-${this.displayEdge.target.id}`);
+
+    // Animate into position
+    TweenLite.fromTo(this.$edge[0], 1,
+      {attr: {x2: this.displayEdge.source.x, y2: this.displayEdge.source.y}},
+      {attr: {x2: this.displayEdge.target.x, y2: this.displayEdge.target.y}, ease: Power1.easeIn}
+    );
   }
 }
