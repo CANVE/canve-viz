661bb24cceff8a7c1eb3ab7698212efcb249c121
Daniela Baron <baron.daniela@gmail.com>
2016-02-13 19:28:27 -0500

    upgrade aurelia and other deps

--- src/graph/action-manager.js
+++ src/graph/action-manager.js
@@ -1,5 +1,6 @@
 import {inject} from 'aurelia-framework';
-import UndoManager from 'npm:undo-manager@1.0.4/undomanager.js';
+// import UndoManager from 'npm:undo-manager@1.0.4/undomanager.js';
+import UndoManager from 'undo-manager/undomanager';
 
 @inject(UndoManager)
 export class ActionManager {
